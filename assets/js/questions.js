1b8b1902d3086cca56c384852dab488220e74539
EmiliaVel <velascomarie10@gmail.com>
2021-12-11 16:37:14 -0500

    quizchallenge

---
+++ assets/js/questions.js
@@ -0,0 +1,26 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "What is the biggest animal in the world?:",
+    choices: ["giraffe", "rihno", "elephant", "blue whale"],
+    answer: "Blue whale"
+  },
+  {
+    title:
+      "Brie chesee is originally from:",
+    choices: ["France", "UK", "Italy", "Switzerland"],
+    answer: "France"
+  },
+  {
+    title: "What was the most streamed show on Netflix in 2020?",
+    choices: ["House of Cards", "The Crown", "Friends", "The Umbrella Academy"],
+    answer: "The Umbrella Academy"
+  },
+  {
+    title:
+      "Where was the mojito cocktail created",
+    choices: ["Mexico", "Cuba", "Brazil", "Barbados"],
+    answer: "Cuba"
+  },
+ 
+];
